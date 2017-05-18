06f6f27e505c02267db08b217569a4b19e9945e6
Luke Pafford <lukepafford@gmail.com>
2017-05-18 02:46:51 -0700

    Removed Redhat6 from STIGS init.pp because Puppet STIGS for Redhat 6 won't be implemented

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,9 +5,6 @@ class stigs {
        if $facts['os']['release']['major'] == '7' {
          include stigs::redhat7::redhat7
        }
-       elsif $facts['os']['release']['major'] == '6' {
-         include stigs::redhat6::redhat6
-       }
     }
     default: {
       warning('This stigs module is not supported on this OS')
